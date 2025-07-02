17118b56e936be01a845e256ea40dd2e6a5cd06a
henrynguyen50 <henrynguyen17@gmail.com>
2025-07-02 10:21:15 -0500

    WOrking autofill hardcode

--- scripts/content.js
+++ scripts/content.js
@@ -1,5 +1,15 @@
+function autofillCompany(className, info) {
+  const input = document.querySelector(className);
+  //validate if page has loaded then set value
+  if (input) {
+    input.value = info;
+    
+  } else {
+    // Retry in 500ms if not found
+    setTimeout(() => autofillCompany(className, info), 500);
+  }
+}
 
+classes = [".cw_company", ".cw_companyPhone", ".cw_companyAdress1", ".cw_companyCity", ".cw_companyState", ".cw_companyZipCode", ".cw_companyId", ".cw_firstName", ".cw_lastName", ".cw_email", ".cw_phone"]
 
-<script>
-    document.querySelector(".cw_company").value = "Henry Nguyen";
-</script>
+autofillCompany(".cw_company", "Henry Nguyen");
