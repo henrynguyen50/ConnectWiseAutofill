0feb8cfdfcec99a26179a221c6e098e87d26c14b
henrynguyen50 <henrynguyen17@gmail.com>
2025-07-02 12:08:03 -0500

    Moved setting local storage to when press button and added splitting name

--- popup.js
+++ popup.js
@@ -4,18 +4,25 @@
 
 //on click init hashmap just like in manifest
 document.getElementById("submitBtn").addEventListener("click", ()=> {
+    //Split name
+    let name = document.getElementById("name").value;
+    const firstLast = name.split(" ");
+
     //.value gets text inside Id element
     const data = {
     ".cw_company": document.getElementById("name").value,
+    ".cw_firstName": firstLast[0],
+    ".cw_lastName": firstLast[1],
     ".cw_email": document.getElementById("email").value,
     ".cw_companyAddress1": document.getElementById("address").value,
     ".cw_companyCity": document.getElementById("city").value,
     ".cw_companyState": document.getElementById("state").value,
     ".cw_companyZipCode": document.getElementById("zip").value,
-    ".cw_companyCountry": document.getElementById("country").value,
     ".cw_companyPhone": document.getElementById("phone").value,
   };
+    //save data to autofillData in chromes local storage 
+    chrome.storage.local.set({autofillData: data}).then(() => {
+    console.log("Data saved");
+})
 })
 
-//save data to autofillData in chromes local storage 
-chrome.storage.local.set({autofillData: data})
